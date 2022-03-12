7e28a28f65d878c1b2257a972caf759f3be6ca93
n-roberge <58243482+n-roberge@users.noreply.github.com>
2022-03-11 20:21:17 -0600

    added wallet functionality

--- atm.js
+++ atm.js
@@ -1,15 +1,25 @@
 //import account.js
 const account = require('./account');
 
+//import wallet.js
+const wallet = require('./wallet');
+
 //prompt-sync module
 const prompt = require('prompt-sync')();
 
+//global balance and wallet
 let {balance} = account.account;
+let {balance:walletBalance} = wallet.wallet;
 
 //Balance function
 function getBalance(){
     console.log("Your current balance is: " + "$" + balance);
     return balance;
+}
+
+//check wallet
+function checkWallet(){
+    console.log(`Current amount in wallet: $${walletBalance}`);
 }
 
 //withdraw function
@@ -20,26 +30,42 @@ function withdraw(){
     console.log(`Withdrawing: $${withdrawlAmount}.`);
 
     //calculates new balance
-    let withdrawl = balance - withdrawlAmount;
-    balance = withdrawl;
+    balance = balance - withdrawlAmount;
 
     //displays new balance
     getBalance();
+
+    //add money to wallet
+    walletBalance = walletBalance + withdrawlAmount;
+    checkWallet();
 };
 
 //deposit function
 function deposit(){
+    
     //deposit to account.js
     console.log("Please enter the amount to deposit: ");
     const depositAmount = Number(promptFor("",amountValid));
-    console.log(`Depositing: $${depositAmount}.`);
 
-    //calculates new balance
-    let deposit = balance + depositAmount;
-    balance = deposit;
+    //deposits if there is enough money in your wallet
+    if (walletBalance >= depositAmount){
+        console.log(`Depositing: $${depositAmount}.`);
 
-    //displays new balance
-    getBalance();
+        //calculates new balance
+        let deposit = balance + depositAmount;
+        balance = deposit;
+
+        //displays new balance
+        getBalance();
+
+        //deposit money from wallet
+        walletBalance = walletBalance - depositAmount;
+        checkWallet();
+    }
+    else {
+        console.log("Somehow the ATM knows you do not have enough money in your wallet to make this deposit.");
+        checkWallet();
+    };
 };
 
 //validates the PIN function
--- wallet.js
+++ wallet.js
@@ -1,6 +1,12 @@
-"use strict";
-
-
 //bonus
-
 //contains and receives cash for ATM deposit and withdraw functions
+
+//import atm.js
+const atm = require('./atm');
+
+var wallet = {
+    balance: 35
+};
+
+//export wallet
+module.exports.wallet = wallet
